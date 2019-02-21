5b8cad90b51c79b3577ee77179125cf614af0db8
zwp <303116854@qq.com>
2019-02-21 15:27:32 +0800

    1.1

--- zwf/routers/login.js
+++ zwf/routers/login.js
@@ -7,14 +7,13 @@ var router=express.Router();
 router.post('/log',(req,res)=>{
     //获取post请求的数据
         //console.log(1111)
-   var $user=req.body.inputUser;
-   var $pwd=req.body.inputPwd;
+   var $uname=req.body.inputUser;
 	//执行SQL语句，将注册的数据插入到xz_user数据表中，
 	//成功响应{code:200,msg:'register suc'}
     //res.send('注册成功');
 
-  var sql="select * from zwf_user where user=? and pwd=?";
-  pool.query(sql,[$user,$pwd],(err,result)=>{
+  var sql="select * from zwf_login where uname=?";
+  pool.query(sql,[$uname],(err,result)=>{
     if (err)  throw err;
    // res.send(result)
     if(result.length>0){res.send("1")}
