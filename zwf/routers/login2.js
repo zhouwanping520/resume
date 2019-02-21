0e809878b08923b5c1153d7046aebc3d42bc995b
zwp <303116854@qq.com>
2019-02-21 11:23:02 +0800

    1.1

--- zwf/routers/login2.js
+++ zwf/routers/login2.js
@@ -7,18 +7,14 @@ var router=express.Router();
 router.get('/',(req,res)=>{
     //获取post请求的数据
         //console.log(1111)
-   var $uname=req.query.uname;
-  if(!$uname){
-    res.send("用户名不能为空");
-    return;
-  }
-  var sql="select * from zwf_login where uname=?";
-  pool.query(sql,[$uname],(err,result)=>{
+   var $phone=req.query.phone;
+  var sql="select * from zwf_login where phone=?";
+  pool.query(sql,[$phone],(err,result)=>{
     if (err)  throw err;
     //res.send(result)
    //console.log(result)
     if(result.length>0){res.send("1")}
-    else{res.send("0")} 
+   else{res.send("0")} 
   })
 });
 module.exports=router;
